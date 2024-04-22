cefc1e16bc9e1a9739fc3761c866381e75079940
rcoelhoweb <rcoelhoweb@gmail.com>
2024-04-22 13:27:10 -0400

    Hello World in C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World"); //read//
+
+            Console.ReadLine();
+        }
+    }
+}
